817059664be3e1f4775f52398753c14d75590735
ovresko <32199620+ovresko@users.noreply.github.com>
2019-12-23 15:39:34 +0100

    Update supplier_quotation_item.py

--- erpnext/buying/doctype/supplier_quotation_item/supplier_quotation_item.py
+++ erpnext/buying/doctype/supplier_quotation_item/supplier_quotation_item.py
@@ -9,5 +9,7 @@ from frappe.model.document import Document
 from erpnext.controllers.print_settings import print_settings_for_item_table
 
 class SupplierQuotationItem(Document):
+	def validate(self):
+		self.ref_devis = self.parent
 	def __setup__(self):
 		print_settings_for_item_table(self)
