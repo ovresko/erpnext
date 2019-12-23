7e989b9efdbbaf6edc021d10391fa8a99a0ccfa7
ovresko <32199620+ovresko@users.noreply.github.com>
2019-12-23 19:45:23 +0100

    Update supplier_quotation_item.js

--- erpnext/buying/doctype/supplier_quotation_item/supplier_quotation_item.js
+++ erpnext/buying/doctype/supplier_quotation_item/supplier_quotation_item.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Supplier Quotation Item', {
-	refresh: function(frm) {
+	before_save: function(frm) {
 		frm.set_value("ref_devis",frm.doc.parent);
 	}
 });
