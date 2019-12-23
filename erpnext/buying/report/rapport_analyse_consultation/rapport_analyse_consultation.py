7d1a186804f65fd402b174ecc3438e64baa8cd5b
ovresko <32199620+ovresko@users.noreply.github.com>
2019-12-23 23:13:02 +0100

    Update rapport_analyse_consultation.py

--- erpnext/buying/report/rapport_analyse_consultation/rapport_analyse_consultation.py
+++ erpnext/buying/report/rapport_analyse_consultation/rapport_analyse_consultation.py
@@ -6,4 +6,15 @@ import frappe
 
 def execute(filters=None):
 	columns, data = [], []
+	columns.append({
+			"fieldname": "item_code",
+			"label": _("Item Code"),
+			"fieldtype": "Link",
+			"options": "Item"
+		})
+	
+	mris = frappe.get_all("Material Request Item",filters={"docstatus":1,"material_request_type": "Purchase","consulted" : 0,"status": ("!=", "Stopped")}, fields=["name","item_code","item_name"])
+	for mri in mris:
+		data.append([mri.item_code])
+		
 	return columns, data
