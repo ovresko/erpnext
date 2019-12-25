f41f68cc63c128fb91fa09e854e54b1ea4277f36
ovresko <32199620+ovresko@users.noreply.github.com>
2019-12-25 22:22:02 +0100

    Update rapport_analyse_consultation.py

--- erpnext/buying/report/rapport_analyse_consultation/rapport_analyse_consultation.py
+++ erpnext/buying/report/rapport_analyse_consultation/rapport_analyse_consultation.py
@@ -9,9 +9,9 @@ from erpnext.stock.get_item_details import get_item_details
 def execute(filters=None):
 	
 	columns, data = [], []
-	if not filters.consultation and not filters.demande:
-		frappe.msgprint("Selectionner une consultation ou une demande de materiel")
-		return columns, data
+	#if not filters.consultation and not filters.demande:
+	#	frappe.msgprint("Selectionner une consultation ou une demande de materiel")
+	#	return columns, data
 	columns.append({
 			"fieldname": "item_code",
 			"label": _("Item Code"),
@@ -133,7 +133,7 @@ def execute(filters=None):
 	mris = []
 	if filters.demande:
 		mris = frappe.get_all("Material Request Item",
-				      filters={"creation":(">=",filters.from_date),"parent":filters.demande,"docstatus":1},
+				      filters={"ordered_qty":0,"creation":(">=",filters.from_date),"parent":filters.demande,"docstatus":1},
 				      fields=["model","qty","last_purchase_rate","max_order_qty","projected_qty","actual_qty","stock_qty","ordered_qty","name","item_code","item_name","parent","consultation","fabricant","ref_fabricant"])
 	#if filters.fabricant:
 	#	mris = frappe.get_all("Material Request Item",
@@ -141,10 +141,10 @@ def execute(filters=None):
 	#			      fields=["model","qty","last_purchase_rate","max_order_qty","projected_qty","actual_qty","stock_qty","ordered_qty","name","item_code","item_name","parent","consultation","fabricant","ref_fabricant"])
 	elif filters.consultation:
 		mris = frappe.get_all("Material Request Item",
-				      filters={"creation":(">=",filters.from_date),"consultation":filters.consultation,"docstatus":1}, fields=["model","qty","last_purchase_rate","max_order_qty","projected_qty","actual_qty","stock_qty","ordered_qty","name","item_code","item_name","parent","consultation","fabricant","ref_fabricant"])
+				      filters={"ordered_qty":0,"creation":(">=",filters.from_date),"consultation":filters.consultation,"docstatus":1}, fields=["model","qty","last_purchase_rate","max_order_qty","projected_qty","actual_qty","stock_qty","ordered_qty","name","item_code","item_name","parent","consultation","fabricant","ref_fabricant"])
 	else:
 		mris = frappe.get_all("Material Request Item",
-				      filters={"creation":(">=",filters.from_date),"docstatus":1},
+				      filters={"ordered_qty":0,"creation":(">=",filters.from_date),"docstatus":1},
 				      fields=["model","qty","last_purchase_rate","max_order_qty","projected_qty","actual_qty","stock_qty","ordered_qty","name","item_code","item_name","parent","consultation","fabricant","ref_fabricant"])
 
 	for mri in mris:
