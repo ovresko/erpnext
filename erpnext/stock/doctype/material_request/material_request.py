3b1a5435f4b5c750b6a398fdd9b1e45c6e600959
ovresko <32199620+ovresko@users.noreply.github.com>
2020-01-16 12:18:41 +0100

    Update material_request.py

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -28,6 +28,18 @@ class MaterialRequest(BuyingController):
 
 	def check_if_already_pulled(self):
 		pass
+	
+	def handle_per_consulted(self):
+		if self.docstatus != 1:
+			return
+		per = 0
+		item_count = len(self.items)
+		consulted = sum(1 for i in self.items if i.consulted == 1)
+		if consulted > 0:
+			per = consulted / item_count
+		result = per * 100
+		frappe.db.sql("""update `tabMaterial Request` set per_consulted = %s
+			where name = %s""" % (result, self.name))
 
 	def validate_qty_against_so(self):
 		so_items = {} # Format --> {'SO/00001': {'Item/001': 120, 'Item/002': 24}}
