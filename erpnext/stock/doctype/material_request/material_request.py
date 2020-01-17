a291ca5bff77ea46225a646179e61b275e8c58fa
ovresko <32199620+ovresko@users.noreply.github.com>
2020-01-17 21:00:23 +0100

    Update material_request.py

--- erpnext/stock/doctype/material_request/material_request.py
+++ erpnext/stock/doctype/material_request/material_request.py
@@ -39,7 +39,7 @@ class MaterialRequest(BuyingController):
 			per = float(consulted) / float(item_count)
 		result = float(per * 100)
 		self.per_consulted = result
-		#frappe.db.sql("""update `tabMaterial Request` set per_consulted = %s where name = %s""", (result, self.name))
+		frappe.db.sql("""update `tabMaterial Request` set per_consulted = %s where name = %s""", (result, self.name))
 
 	def validate_qty_against_so(self):
 		so_items = {} # Format --> {'SO/00001': {'Item/001': 120, 'Item/002': 24}}
