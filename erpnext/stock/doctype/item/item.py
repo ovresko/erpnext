e0875ca60f1e5c0667f4d0c8f399514673ec9487
ovresko <32199620+ovresko@users.noreply.github.com>
2020-01-29 19:03:16 +0100

    Update item.py

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -1093,3 +1093,74 @@ def update_variants(variants, template, publish_progress=True):
 		count+=1
 		if publish_progress:
 				frappe.publish_progress(count*100/len(variants), title = _("Updating Variants..."))
+
+@frappe.whitelist()
+def bulk_print_memberships(names):
+	if len(names) == 0:
+		frappe.msgprint("No rows selected.")
+	
+	final_html = prepare_bulk_print_html(names)
+
+	pdf_options = { 
+					"page-height" : "25.4cm",
+					"page-width" : "17.5cm",
+					"margin-top": "0mm",
+					"margin-bottom": "0mm",
+					"margin-left": "0mm",
+					"margin-right": "0mm",
+					"no-outline": None,
+					"encoding": "UTF-8",
+					"title": "Catalogue"
+				}
+		
+	frappe.local.response.filename = "{filename}.pdf".format(filename="senior_citizen_list".replace(" ", "-").replace("/", "-"))
+	frappe.local.response.filecontent = dignity_get_pdf(final_html, options=pdf_options) #get_pdf(final_html, pdf_options)
+	frappe.local.response.type = "download"
+	
+def prepare_bulk_print_html(names):
+	names = names.split(",")
+
+	#if len(names) > 4:
+	#	frappe.throw("The system cannot print more than 4 Senior Citizen records at a time.")
+
+	html = ""
+	sc_list = []
+
+	for name in names:
+		sc_list.append(frappe.get_doc("Item", name))
+		
+	html_params = { "sc_list": sc_list }
+	final_html = frappe.render_template("erpnext/templates/includes/catalog_bulk_print.html", html_params)
+
+	return final_html
+
+def dignity_get_pdf(html, options=None):
+	fname = os.path.join("/tmp", "dignity-sc-list-{0}.pdf".format(frappe.generate_hash()))
+
+	try:
+		pdfkit.from_string(html, fname, options=options or {})
+
+		with open(fname, "rb") as fileobj:
+			filedata = fileobj.read()
+
+	except IOError, e:
+		if ("ContentNotFoundError" in e.message
+			or "ContentOperationNotPermittedError" in e.message
+			or "UnknownContentError" in e.message
+			or "RemoteHostClosedError" in e.message):
+
+			# allow pdfs with missing images if file got created
+			if os.path.exists(fname):
+				with open(fname, "rb") as fileobj:
+					filedata = fileobj.read()
+
+			else:
+				frappe.throw(_("PDF generation failed because of broken image links"))
+		else:
+			raise
+
+	finally:
+		cleanup(fname)
+
+
+	return filedata
