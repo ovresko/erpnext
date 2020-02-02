bba9220c65def6b493f5610548c2463bfc779b56
ovresko <32199620+ovresko@users.noreply.github.com>
2020-02-02 20:21:10 +0100

    Update rapport_analyse_de_besoin.js

--- erpnext/stock/report/rapport_analyse_de_besoin/rapport_analyse_de_besoin.js
+++ erpnext/stock/report/rapport_analyse_de_besoin/rapport_analyse_de_besoin.js
@@ -5,10 +5,44 @@
 frappe.query_reports["Rapport analyse de besoin"] = {
 	"filters": [
 		{
-		"fieldname": "group",
-		"label": __("Item Group"),
-		fieldtype: "Link",
-		options: "Item Group"
-	}
+			"fieldname": "group",
+			"label": __("Item Group"),
+			fieldtype: "Link",
+			options: "Item Group"
+		},
+		{
+			"fieldname": "modele",
+			"label": __("Modele de vehicule"),
+			fieldtype: "Link",
+			options: "Modele de vehicule"
+		},
+		{
+			"fieldname": "manufacturer",
+			"label": __("Manufacturer"),
+			fieldtype: "Link",
+			options: "Manufacturer",
+			"get_query": function() {
+			return {
+				"doctype": "Manufacturer",
+				"filters": {
+					"actif": 1,
+					   }
+				}
+			}
+		},
+		{
+			"fieldname":"age_plus",
+			"label": __("Age plus que"),
+			"fieldtype": "Select",
+			"options": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
+			"default": ""
+		},
+		{
+			"fieldname":"age_minus",
+			"label": __("Age moin que"),
+			"fieldtype": "Select",
+			"options": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
+			"default": ""
+		}
 	]
 }
