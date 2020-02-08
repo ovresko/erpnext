1353d5d05177cd52e51f8c6cefc9a79c09d7d961
ovresko <32199620+ovresko@users.noreply.github.com>
2020-02-08 17:44:26 +0100

    Update rapport_analyse_de_besoin.py

--- erpnext/stock/report/rapport_analyse_de_besoin/rapport_analyse_de_besoin.py
+++ erpnext/stock/report/rapport_analyse_de_besoin/rapport_analyse_de_besoin.py
@@ -259,7 +259,8 @@ def get_conditions(filters):
 		where vsr.generation_vehicule='%s'))""" % generation)
 
 	if filters.get('price_list'):
-		conditions.append("""(item_code in (select item_code from `tabItem Price` vpr 
+		conditions.append(""" (item_code in (select item_code from `tabItem Price` vpr 
+		where vpr.price_list=%(price_list)s) or variant_of in (select item_model from `tabItem Price` vpr 
 		where vpr.price_list=%(price_list)s))""")
 
 	#if filters.get('modele'):
