c94855212313d8a4b83538290bfad441633b8357
ovresko <32199620+ovresko@users.noreply.github.com>
2020-02-17 13:28:52 +0100

    Update rapport_analyse_devis_fournisseur.py

--- erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.py
+++ erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.py
@@ -719,7 +719,7 @@ def execute(filters=None):
 							if pl.name:
 								price = frappe.db.sql("""select price_list_rate from `tabItem Price` where buying=1 and price_list=%s and (  item_code=%s) ORDER BY creation DESC LIMIT 1;""",(pl.name,mri.item_code))
 								if price:
-									all_prices += "%s : %d %s - " % (pl.name ,price[0][0] or 0,pl.currency)
+									all_prices += "%s : %f %s - " % (pl.name ,price[0][0] or 0,pl.currency)
 						row.append(all_prices)
 
 			data.append(row)
