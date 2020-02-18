d8fab106cfec31b05a6e84664a6f80218d527242
ovresko <32199620+ovresko@users.noreply.github.com>
2020-02-18 21:11:50 +0100

    Update rapport_analyse_devis_fournisseur.py

--- erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.py
+++ erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.py
@@ -419,7 +419,7 @@ def execute(filters=None):
 		asupplier_name = frappe.db.get_value("Supplier Quotation",filters.get("consultation_interne"),"supplier_name")
 		data.append(["","Fournisseur :",asupplier_name or ''])
 	data.append(["","Article Total :",len(mitems) or 0])
-	data.append(["","En cours :",sum(1 for i in mitems if hasattr(i, 'material_request') and i.confirmation == "En cours") or 0])
+	data.append(["","En cours :",sum(1 for i in mitems if hasattr(i, 'material_request') and (i.confirmation == "En cours" or not i.confirmation)) or 0])
 	data.append(["","Approuve :",sum(1 for i in mitems if hasattr(i, 'material_request') and i.confirmation == "Approuve") or 0])
 	data.append(["","Annule :",sum(1 for i in mitems if hasattr(i, 'material_request') and i.confirmation == "Annule") or 0])
 	data.append(["","En negociation :",sum(1 for i in mitems if hasattr(i, 'material_request') and i.confirmation == "En negociation") or 0])
