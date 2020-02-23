278800701bb736cabd592feaf04ebc989f693ac2
ovresko <32199620+ovresko@users.noreply.github.com>
2020-02-23 20:45:00 +0100

    Update rapport_analyse_devis_fournisseur.js

--- erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.js
+++ erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.js
@@ -162,7 +162,7 @@ frappe.query_reports["Rapport analyse devis fournisseur"] = {
 		value = default_formatter(value, row, column, data);
 		
 		
-		if (row && row != null && row[2].content!=null && row[2] && row[2].content.length == 11 && !row[1].content.includes("comp")) {
+		if (row && row != null && row[2].content!=null && row[2] && row[2].content.length == 11 && row[1].content != null) {
 			value = "<div style='color: white;background-color: #008081;padding: 5px;'>" + value + "</div>";
 		}
 		else if(row && row != null && row[2].content!=null && row[2] && row[2].content.length == 11 && row[1].content == null)
