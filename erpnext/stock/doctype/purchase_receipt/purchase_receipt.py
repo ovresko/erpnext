d4ef7ab8ecc74b40f9284904509005de1190643e
ovresko <32199620+ovresko@users.noreply.github.com>
2020-04-23 13:06:37 +0200

    Update purchase_receipt.py

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
@@ -469,6 +469,7 @@ def make_purchase_invoice(source_name, target_doc=None):
 			"doctype": "Purchase Invoice Item",
 			"field_map": {
 				"name": "pr_detail",
+				"parent": "against_puchase_invoice",
 				"parent": "purchase_receipt",
 				"purchase_order_item": "po_detail",
 				"purchase_order": "purchase_order",
