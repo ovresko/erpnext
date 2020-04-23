c2b1a94f7a1388feae1e38307656254445f9b958
ovresko <32199620+ovresko@users.noreply.github.com>
2020-04-23 14:44:40 +0200

    Update purchase_receipt_dashboard.py

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt_dashboard.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt_dashboard.py
@@ -14,6 +14,7 @@ def get_data():
 		'internal_links': {
 			'Purchase Order': ['items', 'purchase_order'],
 			'Project': ['items', 'project'],
+			'Purchase Invoice': ['items','against_purchase_invoice']
 			'Quality Inspection': ['items', 'quality_inspection'],
 		},
 		'transactions': [
