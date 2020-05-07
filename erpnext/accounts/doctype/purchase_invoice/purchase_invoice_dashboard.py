6072c0011b5637f9079391a0e9cec90602d71671
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-07 18:33:07 +0200

    Update purchase_invoice_dashboard.py

--- erpnext/accounts/doctype/purchase_invoice/purchase_invoice_dashboard.py
+++ erpnext/accounts/doctype/purchase_invoice/purchase_invoice_dashboard.py
@@ -7,7 +7,7 @@ def get_data():
 		'non_standard_fieldnames': {
 			'Purchase Receipt': 'against_puchase_invoice',
 			'Journal Entry': 'ref_facture',
-			'Journal Entry': 'reference_name',			
+			#'Journal Entry': 'reference_name',			
 			'Payment Entry': 'reference_name',
 			'Payment Request': 'reference_name',
 			'Landed Cost Voucher': 'receipt_document',
