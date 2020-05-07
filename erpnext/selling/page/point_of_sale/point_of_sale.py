e9704b1d60dd3c36e80aa362d655abcd96137363
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-08 00:30:14 +0200

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -54,8 +54,10 @@ def get_items(start, page_length, price_list, item_group, search_value="", pos_p
 			where
 				i.disabled = 0 and i.has_variants = 0 and i.is_sales_item = 1
 				and i.item_group in (select name from `tabItem Group` where lft >= {lft} and rgt <= {rgt})
-		        	and {condition} order by idx desc limit {start}, {page_length}
+		        	and {condition}
 			GROUP BY i.name
+				order by idx desc limit {start}, {page_length}
+			
 				""".format(start=start,page_length=page_length,lft=lft, rgt=rgt,condition=condition),
 			{
 				'item_code': item_code,
@@ -91,8 +93,10 @@ def get_items(start, page_length, price_list, item_group, search_value="", pos_p
 			where
 				i.disabled = 0 and i.has_variants = 0 and i.is_sales_item = 1
 				and i.item_group in (select name from `tabItem Group` where lft >= {lft} and rgt <= {rgt})
-				and {condition} order by idx desc limit {start}, {page_length}
-			GROUP BY i.name
+				and {condition} 
+				GROUP BY i.name
+				order by idx desc limit {start}, {page_length}
+			
 				""".format
 				(start=start,page_length=page_length,lft=lft, 	rgt=rgt, condition=condition),
 			{
