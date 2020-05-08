ffa9dd6b1828efe1df078d5a053a2ce59f8af248
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-08 18:37:20 +0200

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -57,7 +57,8 @@ def get_items(start, page_length, price_list, item_group, search_value="", pos_p
 
 	if display_items_in_stock == 0:
 		res = frappe.db.sql("""select i.name as item_code, i.item_name, i.image as item_image, i.idx as idx,
-			i.is_stock_item, item_det.price_list_rate, item_det.currency, i.oem_text,i.titre_article,i.manufacturer,i.manufacturer_part_no,i.fabricant_logo ,i.critere_text  
+			i.is_stock_item, item_det.price_list_rate, item_det.currency, i.oem_text,i.titre_article,i.manufacturer,i.manufacturer_part_no,i.fabricant_logo 
+			,i.critere_text  ,MATCH(i.name,i.item_name,i.nom_generique_long,i.manufacturer_part_no) as relevance
 			from `tabItem` i 
 			LEFT JOIN
 				(select item_code, price_list_rate, currency from
@@ -68,7 +69,7 @@ def get_items(start, page_length, price_list, item_group, search_value="", pos_p
 			where
 				i.disabled = 0 and i.has_variants = 0 and i.is_sales_item = 1
 				and i.item_group in (select name from `tabItem Group` where lft >= {lft} and rgt <= {rgt})
-		        	and {condition} order by score desc limit {start}, {page_length}""".format(start=start,page_length=page_length,lft=lft, rgt=rgt,condition=condition),
+		        	and {condition} order by relevance desc limit {start}, {page_length}""".format(start=start,page_length=page_length,lft=lft, rgt=rgt,condition=condition),
 			{
 				'item_code': item_code,
 				'price_list': price_list
@@ -80,7 +81,9 @@ def get_items(start, page_length, price_list, item_group, search_value="", pos_p
 
 	elif display_items_in_stock == 1:
 		query = """select i.name as item_code, i.item_name, i.image as item_image, i.idx as idx,
-				i.is_stock_item, item_det.price_list_rate, item_det.currency, i.oem_text,i.titre_article,i.manufacturer,i.manufacturer_part_no,i.fabricant_logo , i.critere_text 
+				i.is_stock_item, item_det.price_list_rate, item_det.currency, i.oem_text,i.titre_article,
+				i.manufacturer,i.manufacturer_part_no,i.fabricant_logo , i.critere_text 
+				,MATCH(i.name,i.item_name,i.nom_generique_long,i.manufacturer_part_no) as relevance
 				from `tabItem` i LEFT JOIN
 					(select item_code, price_list_rate, currency from
 						`tabItem Price`	where price_list=%(price_list)s) item_det
@@ -100,7 +103,7 @@ def get_items(start, page_length, price_list, item_group, search_value="", pos_p
 			where
 				i.disabled = 0 and i.has_variants = 0 and i.is_sales_item = 1
 				and i.item_group in (select name from `tabItem Group` where lft >= {lft} and rgt <= {rgt})
-				and {condition} order by score desc limit {start}, {page_length}""".format(start=start,page_length=page_length,lft=lft, 	rgt=rgt, condition=condition),
+				and {condition} order by relevance desc limit {start}, {page_length}""".format(start=start,page_length=page_length,lft=lft, 	rgt=rgt, condition=condition),
 			{
 				'item_code': item_code,
 				'price_list': price_list,
@@ -151,7 +154,7 @@ def get_conditions(item_code, serial_no, batch_no, barcode):
 	if serial_no or batch_no or barcode:
 		return frappe.db.escape(item_code), "i.name = %(item_code)s"
 
-	condition = """ (MATCH(i.name,i.item_name,i.nom_generique_long,i.manufacturer_part_no) AGAINST(%(item_code)s IN NATURAL LANGUAGE MODE) AS score """
+	condition = """ MATCH AGAINST(%(item_code)s IN NATURAL LANGUAGE MODE)  """
 
 	return '%%%s%%'%(frappe.db.escape(item_code)), condition
 
