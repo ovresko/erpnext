410d784b1870f6e092132af221c4bbd5b45af182
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-08 18:30:03 +0200

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -68,7 +68,7 @@ def get_items(start, page_length, price_list, item_group, search_value="", pos_p
 			where
 				i.disabled = 0 and i.has_variants = 0 and i.is_sales_item = 1
 				and i.item_group in (select name from `tabItem Group` where lft >= {lft} and rgt <= {rgt})
-		        	and {condition} order by i.score desc limit {start}, {page_length}""".format(start=start,page_length=page_length,lft=lft, rgt=rgt,condition=condition),
+		        	and {condition} order by score desc limit {start}, {page_length}""".format(start=start,page_length=page_length,lft=lft, rgt=rgt,condition=condition),
 			{
 				'item_code': item_code,
 				'price_list': price_list
@@ -100,7 +100,7 @@ def get_items(start, page_length, price_list, item_group, search_value="", pos_p
 			where
 				i.disabled = 0 and i.has_variants = 0 and i.is_sales_item = 1
 				and i.item_group in (select name from `tabItem Group` where lft >= {lft} and rgt <= {rgt})
-				and {condition} order by i.score desc limit {start}, {page_length}""".format(start=start,page_length=page_length,lft=lft, 	rgt=rgt, condition=condition),
+				and {condition} order by score desc limit {start}, {page_length}""".format(start=start,page_length=page_length,lft=lft, 	rgt=rgt, condition=condition),
 			{
 				'item_code': item_code,
 				'price_list': price_list,
