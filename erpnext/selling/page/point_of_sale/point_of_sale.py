03709ebe33c8ea8277c6576015da7bbacc8fd24f
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-09 23:22:00 +0200

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -156,7 +156,7 @@ def get_conditions(item_code, serial_no, batch_no, barcode):
 	words = item_code.split()
 	keyword = '* '.join(w for w in words)
 
-	condition = """ ( i.clean_manufacturer_part_number LIKE '%%{}%%' or i.oem_text LIKE '%%{}%%' or  MATCH(i.name,i.item_name,i.nom_generique_long,i.manufacturer_part_no,i.clean_manufacturer_part_number,i.oem_text) AGAINST('({})' IN NATURAL LANGUAGE MODE)  )""".format(item_code,item_code,keyword)
+	condition = """ ( i.clean_manufacturer_part_number LIKE '%%{}%%' or i.oem_text LIKE '%%{}%%' or  MATCH(i.name,i.item_name,i.nom_generique_long,i.manufacturer_part_no,i.clean_manufacturer_part_number,i.oem_text) AGAINST('({})' IN NATURAL LANGUAGE MODE)  )""".format(item_code,item_code,item_code)
 
 	return '%%%s%%'%(frappe.db.escape(item_code)), condition
 
