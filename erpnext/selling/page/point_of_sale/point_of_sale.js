9d3d99b55106b7a33e8a53e350ae47f74a93387d
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-11 01:34:04 +0200

    Update point_of_sale.js

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -1269,12 +1269,17 @@ class POSItems {
 				<img class="vehicule-image" src="" width="100%">
 				</div>
 
-				
-				<div style="width:50% ">					
-				<span class="strong">Marque</span> : <span class="vehicule-marque-name clearfix"></span> 
-				<span class="strong">Modele</span> : <span class="vehicule-modele-name clearfix"></span>
-				<span class="strong">Generation</span> : <span class="vehicule-generation-name clearfix"></span> 
-				<span class="strong">Version</span> : <span class="vehicule-version-name clearfix"></span> 
+				<div style="width:10% ;padding:10px">	
+				<span class="strong">Marque</span> :
+				<span class="strong">Modele</span> : 
+				<span class="strong">Generation</span> :
+				<span class="strong">Version</span> :
+				</div>
+				<div style="width:50% ;padding:10px">					
+				 <span class="vehicule-marque-name "></span> 
+				<span class="vehicule-modele-name "></span>
+				 <span class="vehicule-generation-name "></span> 
+				 <span class="vehicule-version-name "></span> 
 				</div>
 
 			</div>
