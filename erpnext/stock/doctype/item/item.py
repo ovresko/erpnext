b89916775b8b1ba8d69a56bded967ae4af051db1
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-10 22:15:09 +0200

    Update item.py

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -134,10 +134,10 @@ class Item(WebsiteGenerator):
 			self.critere_text = ' / '.join(str(x) for x in cr)
 		self.composant_text = ""
 		for cmp in self.composant:
-			self.composant_text +=  cmp.manufacturer_part_no +' '+ cmp.item_name +' / '
+			self.composant_text +=  (cmp.manufacturer_part_no or '') +' '+ (cmp.item_name or '') +' / '
 		self.articles_text= ""
 		for art in self.articles:
-			self.articles_text +=  art.manufacturer_part_no +' '+ art.item_name +' / '
+			self.articles_text +=  (art.manufacturer_part_no or '') +' '+ (art.item_name or '') +' / '
 		#critere_text
                 self.oem_text = ""
 		for o in self.oem:
