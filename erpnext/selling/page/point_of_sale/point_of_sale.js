3ffd6c865385951164bf8ad5da65dfe342c6ee85
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-14 01:28:04 +0200

    Update point_of_sale.js

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -1942,7 +1942,7 @@ class POSItems {
 			actual_qty = '<span class="strong" style="color:#02AF22">Qts: '+item.actual_qty+'</span><br>';
 		
 		}else{
-			actual_qty = '<span class="strong" style="color:1810E6">Qts: '+item.actual_qty+'</span>   <button data-item-code="'+item.item_code+'" data-label="stock" class="btn btn-success btn-xs btn-stock" style="margin-right: 5px;"><i class="fa fa-cubes"></i></button>';
+			actual_qty = '<span class="strong" style="color:#1810E6">Qts: 0 </span>   <button data-item-code="'+item.item_code+'" data-label="stock" class="btn btn-default btn-xs btn-stock" style="margin-right: 5px;"><i class="fa fa-cubes"></i></button>';
 				
 		}
 		let critere = '';
