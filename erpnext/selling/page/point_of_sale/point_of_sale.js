ddffd9f5c05ca99d07e9c8f459aa160621b7b3e7
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-14 23:31:21 +0200

    Update point_of_sale.js

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -1108,7 +1108,7 @@ class POSCart {
 		const is_stock_item = this.get_item_details(item.item_code).is_stock_item;
 		const rate = format_currency(item.rate, this.frm.doc.currency,0);
 		const amount = format_currency(item.amount, this.frm.doc.currency,0);
-		
+		console.log("item",item);
 		const indicator_class = (!is_stock_item || item.actual_qty >= item.qty) ? 'green' : 'red';
 		const batch_no = item.batch_no || '';
 
@@ -1116,7 +1116,8 @@ class POSCart {
 			<div class="list-item indicator ${indicator_class}" data-item-code="${escape(item.item_code)}"
 				data-batch-no="${batch_no}" title="Item: ${item.item_name}  Available Qty: ${item.actual_qty}">
 				<div class="item-name list-item__content list-item__content--flex-1.5 ellipsis">
-					${item.item_name}
+					<div>${item.item_name}</div>
+					<div>${item.fabricant} : ${item.ref_rfabricant}</div>
 				</div>
 				<div class=" text-right" style="margin:10px">
 				<div class="quantity list-item__content text-right">
