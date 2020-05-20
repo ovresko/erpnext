9b41aa4107a0b65fb972d96b02dfd6891cbec1f8
ovresko <32199620+ovresko@users.noreply.github.com>
2020-05-20 23:10:40 +0000

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -256,7 +256,7 @@ def get_item_manufacturer(item_manufacturer):
 	return cond
 
 def get_item_oem(item_oem):
-	frappe.msgprint(item_oem)
+	#frappe.msgprint(item_oem)
 	cond = """ and i.oem_text LIKE '%%{}%%' """.format(item_oem)
 	return cond
 
