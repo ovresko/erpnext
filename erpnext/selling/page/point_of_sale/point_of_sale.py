0e96dbd24d311abdb79233d5d673a2454c00a16d
ovresko <32199620+ovresko@users.noreply.github.com>
2020-06-09 19:23:14 +0100

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -378,10 +378,11 @@ def get_conditions(item_code, serial_no, batch_no, barcode):
 	if not item_code:
 		return ""," 1=1 "
 	words = item_code.split()
-	keyword = '* '.join(w for w in words)
+	keyword = '* +'.join(w for w in words)
+	keyword = "+%s" % keyword
 
 	#condition = """ ( i.clean_manufacturer_part_number LIKE '%%{}%%' or i.oem_text LIKE '%%{}%%' or  MATCH(i.name,i.item_name,i.nom_generique_long,i.manufacturer_part_no,i.clean_manufacturer_part_number,i.oem_text) AGAINST('({})' IN NATURAL LANGUAGE MODE)  )""".format(item_code,item_code,item_code)
-	condition = """ (  i.item_code = '{}' or i.item_code LIKE '%%{}%%' or i.clean_manufacturer_part_number = '{}' or i.clean_manufacturer_part_number LIKE '%%{}%%' or  i.manufacturer_part_no = '{}' or  i.manufacturer_part_no LIKE '%%{}%%' or i.oem_text = '{}'  or i.oem_text LIKE '%%{}%%' or MATCH(i.nom_generique_long) AGAINST('({}) ({})' IN NATURAL LANGUAGE MODE)  )""".format(item_code,item_code,item_code,item_code,item_code,item_code,item_code,item_code,keyword,item_code)
+	condition = """ (  i.item_code = '{}' or i.item_code LIKE '%%{}%%' or i.clean_manufacturer_part_number = '{}' or i.clean_manufacturer_part_number LIKE '%%{}%%' or  i.manufacturer_part_no = '{}' or  i.manufacturer_part_no LIKE '%%{}%%' or i.oem_text = '{}'  or i.oem_text LIKE '%%{}%%' or MATCH(i.nom_generique_long) AGAINST('({}) ({})' IN BOOLEAN MODE)  )""".format(item_code,item_code,item_code,item_code,item_code,item_code,item_code,item_code,keyword,item_code)
 
 	return '%%%s%%'%(frappe.db.escape(item_code)), condition
 
