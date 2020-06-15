fd1b25ccf0c6f686692900d4ac6de0b137d3ed03
ovresko <32199620+ovresko@users.noreply.github.com>
2020-06-15 17:25:02 +0100

    Update rapport_analyse_devis_fournisseur.js

--- erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.js
+++ erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.js
@@ -13,13 +13,14 @@ function open_item_info(item_code) {
 					},
 					"callback": function(response) {
 						var item = response.message; 
-						console.log(item);
+						
 						if (item) {
 							if(me.msg_information)
 							{
 								//$(me.msg_information).remove();
 								me.msg_information = null;
 							}
+							console.log(me.msg_information);
 							me.msg_information = frappe.msgprint(
 								`
 								<button type="button" data-item-code="${item_code}" class="btn btn-primary btn-sm btn-versions-list" > 
@@ -180,7 +181,7 @@ function open_item_info(item_code) {
 									});
 										 
 							});
-							 
+							 me.msg_information.show();
 						}  
 					}
 				}); 	
