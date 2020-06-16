e166e231925cb77fa9822a7d4393c12559279a33
ovresko <32199620+ovresko@users.noreply.github.com>
2020-06-16 20:46:14 +0100

    Update rapport_analyse_devis_fournisseur.py

--- erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.py
+++ erpnext/stock/report/rapport_analyse_devis_fournisseur/rapport_analyse_devis_fournisseur.py
@@ -660,7 +660,7 @@ def execute(filters=None):
 			_date = _recom[0].modified
 				#date = frappe.utils.get_datetime(date).strftime("%d/%m/%Y")
 
-		comp = """<button   onClick="open_item_info('%s')" type='b'> info </button> &nbsp;&nbsp;&nbsp; <button   onClick="achat_item('%s')" type='a'> X </button>""" % (mri.item_code,mri.name)
+		comp = """<button   onClick="open_item_info('%s')" type='b'> info </button>""" % (mri.item_code,mri.name)
 		if len(mri.item_code) == 11 and mri.item_code not in _models:
 			comp = None
 		if is_full:
