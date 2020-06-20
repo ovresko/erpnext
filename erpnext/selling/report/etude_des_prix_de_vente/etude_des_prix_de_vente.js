4a1a97bbf1e13e20f73213b5f33a3c4609636539
ovresko <32199620+ovresko@users.noreply.github.com>
2020-06-20 17:45:12 +0200

    Update etude_des_prix_de_vente.js

--- erpnext/selling/report/etude_des_prix_de_vente/etude_des_prix_de_vente.js
+++ erpnext/selling/report/etude_des_prix_de_vente/etude_des_prix_de_vente.js
@@ -375,7 +375,7 @@ frappe.query_reports["Etude des prix de vente"] = {
 				return {
 					"doctype": "Price List",
 					"filters": {
-						"enabled":("!=","2")
+						"enabled": ("in",[0,1])
 					}
 				}
 			}
