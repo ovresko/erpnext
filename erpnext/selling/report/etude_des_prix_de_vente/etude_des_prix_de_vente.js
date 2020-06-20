be50810523156e1047e063ec382eaa56ff97e862
ovresko <32199620+ovresko@users.noreply.github.com>
2020-06-20 18:03:16 +0200

    Update etude_des_prix_de_vente.js

--- erpnext/selling/report/etude_des_prix_de_vente/etude_des_prix_de_vente.js
+++ erpnext/selling/report/etude_des_prix_de_vente/etude_des_prix_de_vente.js
@@ -257,7 +257,7 @@ frappe.query_reports["Etude des prix de vente"] = {
 		
 		 
 		if (data!=null && data["item_code"] &&  data["item_code"].length == 11 ) {
-			value = "<div style='color: white;background-color: #008081;padding: 0px;'>" + value + "</div>";
+			value = "<div style='color: white;background-color: #008081;padding: 5px;'>" + value + "</div>";
 		}
 		if (data!=null && data['prix_traite'].includes("Approuve")) {
 			value = "<div style='color:#008000;padding: 0px;'>" + value + "</div>";
