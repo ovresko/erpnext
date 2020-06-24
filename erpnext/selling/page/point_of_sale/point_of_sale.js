76fdf3689c5c4ddaf7f3994034b2300ec2b04000
ovresko <32199620+ovresko@users.noreply.github.com>
2020-06-24 18:58:49 +0100

    Update point_of_sale.js

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -2631,7 +2631,7 @@ class POSItems {
 		
 		//btn-dosearch
 		this.wrapper.on('click', '.btn-dosearch', function(event) {
-			 var keyw = me.search_field.val();
+			 var keyw = me.search_field.find('input:text').val();
 			console.log("keyw",keyw);
 			me.start = 0;
 			me.filter_items({ start:me.start,search_term: keyw});
