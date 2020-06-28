cebf0d4e74ec5a6021a91f7e0aa740c90b63d066
ovresko <32199620+ovresko@users.noreply.github.com>
2020-06-28 14:50:24 +0200

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -83,7 +83,8 @@ def open_item_info(item_code):
 						<div>{composant_text}</div>
 						<hr>""".format(item_name=item.item_name,image=image,item_code=item_code,manufacturer_part_no=item.manufacturer_part_no
 							    ,oem_text=item.oem_text,manufacturer=item.manufacturer,fabricant_logo=fabricant_logo,critere_text=item.critere_text,articles_text=item.articles_text,composant_text=item.composant_text)
-		frappe.msgprint(_modal)
+		d = frappe.msgprint(_modal)
+		return d
 		
 			
 
