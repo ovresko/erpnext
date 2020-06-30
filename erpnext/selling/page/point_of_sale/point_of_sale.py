2b86e40abb8ec0bbf9da5d838553566d52bded1d
ovresko <32199620+ovresko@users.noreply.github.com>
2020-06-30 14:30:11 +0200

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -26,7 +26,7 @@ def open_item_info(item_code):
 						<button type="button" data-item-code="{item_code}" class="btn btn-primary btn-sm btn-versions-list" > 
 							<span class="hidden-xs">Vehicules Supportees</span>
 						</button>
-						<button  class="btn btn-default btn-xs btn-open" data-item-code="{item_code}"  style="margin-right: 5px;"><i class="fa fa-eye">
+						<button  class="btn btn-default btn-sm  btn-open" data-item-code="{item_code}"  style="margin-right: 5px;"><i class="fa fa-eye">
 						</i>  Ouvrir Détails</button>
 <br>
 						<table class="table table-bordered table-condensed">
