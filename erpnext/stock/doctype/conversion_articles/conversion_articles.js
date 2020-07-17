8fefa01989290cdd89854c71e00e7a5552a62fb1
ovresko <32199620+ovresko@users.noreply.github.com>
2020-07-17 20:47:39 +0100

    Update conversion_articles.js

--- erpnext/stock/doctype/conversion_articles/conversion_articles.js
+++ erpnext/stock/doctype/conversion_articles/conversion_articles.js
@@ -11,14 +11,15 @@ frappe.ui.form.on('Conversion Articles', {
 		for(var i = 0;i < lines.length;i++){
 		    	//code here using lines[i] which will give you each line
 			var ref = lines[i];
-			
+			console.log("red",ref);
 			if(ref){
 				var clean = ref.replace(/ /g,"").replace(/-/g,"").replace(/_/g,"").replace(/\//g,"").replace(/./g,"");
 				if(clean){
+					console.log("clean",clean);
 					frappe.db.get_value('Item', {clean_manufacturer_part_number: clean}, ['item_code'], (r) => {
-					if (r) {	
-						var old = frm.doc.codes;
+					if (r) {							
 						old += "\n"+r.item_code; 
+						console.log("item_code",r.item_code);
 					}
 					});
 				}
