b78361a49988c376e38acf2593cbf3ce47602ec4
ovresko <32199620+ovresko@users.noreply.github.com>
2020-07-17 21:42:41 +0100

    Update conversion_articles.py

--- erpnext/stock/doctype/conversion_articles/conversion_articles.py
+++ erpnext/stock/doctype/conversion_articles/conversion_articles.py
@@ -8,3 +8,19 @@ from frappe.model.document import Document
 
 class ConversionArticles(Document):
 	pass
+
+def get_converstion(refs):
+	result = ""
+	if refs:
+		clean = refs.splitlines()		
+		if clean:
+			for c in clean:
+				if c:
+					c = c.replace(" ","").replace("-","").replace("_","").replace("/","").replace(".","")
+					code = frappe.db.get_value('Item', {clean_manufacturer_part_number: c}, ['item_code'])
+					result += "%s\n" % (code or '')
+				else:
+					
+					result += "\n"
+	return result
+					
