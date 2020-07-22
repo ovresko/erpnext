0745d48230ce2272ee28b83a28ff5e04d06e64e1
ovresko <32199620+ovresko@users.noreply.github.com>
2020-07-22 22:57:51 +0100

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -20,6 +20,7 @@ from six import string_types
 def get_customer(customer):
 	cs = frappe.get_doc("Customer",customer)
 	bl = get_balance_on(party_type="Customer",party=customer)
+	frappe.msgprint("%s" % bl)
 	cs.update({"balance":bl or '0.00'})
 	return cs
 	
