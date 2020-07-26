ac2721e6430c1db2fabaf79601164630cfd6ee63
ovresko <32199620+ovresko@users.noreply.github.com>
2020-07-26 16:51:42 +0200

    Update articles_a_transferes.py

--- erpnext/selling/report/articles_a_transferes/articles_a_transferes.py
+++ erpnext/selling/report/articles_a_transferes/articles_a_transferes.py
@@ -6,4 +6,78 @@ import frappe
 
 def execute(filters=None):
 	columns, data = [], []
+	columns.append({
+			"fieldname": "item",
+			"label": "Article",
+			"width": 200
+		})
+	columns.append({
+			"fieldname": "item_ref",
+			"label": "Reference",
+			"width": 150
+		})
+	columns.append({
+			"fieldname": "fabricant",
+			"label": "Fabricant",
+			"width": 150
+		})
+	columns.append({
+			"fieldname": "document",
+			"label": "Transfere pour",
+			"width": 150
+		})
+	columns.append({
+			"fieldname": "warehouse",
+			"label": "Stock",
+			"width": 150
+		})
+	columns.append({
+			"fieldname": "qty_total",
+			"label": "Qts besoin total",
+			"width": 150
+		})
+	columns.append({
+			"fieldname": "qty_disp",
+			"label": "Qts disponible",
+			"width": 150
+		})
+	columns.append({
+			"fieldname": "qty",
+			"label": "Qts a transferee",
+			"width": 150
+		})
+	columns.append({
+			"fieldname": "source",
+			"label": "Stock Source Suggere",
+			"width": 150
+		})
+	columns.append({
+			"fieldname": "qty_source",
+			"label": "Qts dans Stock Source",
+			"width": 150
+		})
+	columns.append({
+			"fieldname": "traite",
+			"label": "Qts transferee",
+			"width": 150
+		})
+	
+	orders_items = frappe.db.sql(""" select * from `tabSales Order Item` soi where soi.docstatus=1 and soi.delivered_qty=0 and soi.actual_qty < soi.qty 
+	and soi.parent not in (select so.name from `tabSales Order` so where so.docstatus = 1 and so.status in ('Closed','Cancelled','Draft')) """,as_dict=1)
+	for item in orders_items:
+		row = [
+			item.item_code,
+			item.ref_fabricant,
+			item.fabricant,
+			item.parent,
+			item.warehouse,
+			item.qty,
+			item.actual_qty,
+			(item.qty - item.actual_qty),
+			"",
+			"",
+			""
+		]
+	
+	data.append(row)
 	return columns, data
