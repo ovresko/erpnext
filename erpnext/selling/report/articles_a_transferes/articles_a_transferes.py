1145e4b24383a0a006ba86c1611395f92a4d9e9f
ovresko <32199620+ovresko@users.noreply.github.com>
2020-07-26 16:56:32 +0200

    Update articles_a_transferes.py

--- erpnext/selling/report/articles_a_transferes/articles_a_transferes.py
+++ erpnext/selling/report/articles_a_transferes/articles_a_transferes.py
@@ -68,7 +68,7 @@ def execute(filters=None):
 		})
 	
 	orders_items = frappe.db.sql(""" select * from `tabSales Order Item` soi where soi.docstatus=1 and soi.delivered_qty=0 and soi.actual_qty < soi.qty 
-	and soi.parent not in (select so.name from `tabSales Order` so where so.docstatus = 1 and so.status in ('Closed','Cancelled','Draft')) """,as_dict=1)
+	and soi.parent in (select so.name from `tabSales Order` so where so.docstatus = 1 and so.workflow_state=="Reservation" and so.status not in ('Closed','Cancelled','Draft')) """,as_dict=1)
 	for item in orders_items:
 		row = [
 			item.item_code,
