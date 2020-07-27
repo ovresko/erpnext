de641142a87fc8fdef45c9cb2f880655e1068b61
ovresko <32199620+ovresko@users.noreply.github.com>
2020-07-27 21:40:02 +0100

    Update articles_a_transferes.py

--- erpnext/selling/report/articles_a_transferes/articles_a_transferes.py
+++ erpnext/selling/report/articles_a_transferes/articles_a_transferes.py
@@ -149,11 +149,17 @@ def execute(filters=None):
 			total_qty = item.qty
 		added.append(item.item_code)
 		
-		suggere_qty = frappe.db.sql("""select warehouse,actual_qty from `tabBin` where item_code=%s and actual_qty>%s  and warehouse!=%s""",(item.item_code,qts_transfere,item.warehouse))[0]
-		if not suggere_qty :
-			suggere_qty = frappe.db.sql("""select warehouse,actual_qty from `tabBin` where item_code=%s    and warehouse!=%s order by actual_qty desc""",(item.item_code, item.warehouse))[0]
-			if suggere_qty:
+		suggere_qty = frappe.db.sql("""select warehouse,actual_qty from `tabBin` where item_code=%s and actual_qty>%s  and warehouse!=%s""",(item.item_code,qts_transfere,item.warehouse))
+		if not suggere_qty and not suggere_qty.actual_qty :
+			suggere_qty = frappe.db.sql("""select warehouse,actual_qty from `tabBin` where item_code=%s    and warehouse!=%s order by actual_qty desc""",(item.item_code, item.warehouse))
+			if suggere_qty and suggere_qty[0]:
+				suggere_qty = suggere_qty[0]
 				suggere_qty = "Qts Insufisante | %s %s" % (suggere_qty.warehouse,suggere_qty.actual_qty)
+			else:
+				suggere_qty = "Non disponible"
+		else:
+			suggere_qty = "%s | %s" % (suggere_qty.warehouse,suggere_qty.actual_qty)
+			
 				
 		row = [
 			delivery_date,
