4add6b5a9a51bd562f6fb3f60b4968fa9cc32196
ovresko <32199620+ovresko@users.noreply.github.com>
2020-08-04 14:36:04 +0100

    Update conversion_articles.py

--- erpnext/stock/doctype/conversion_articles/conversion_articles.py
+++ erpnext/stock/doctype/conversion_articles/conversion_articles.py
@@ -17,70 +17,74 @@ class ConversionArticles(Document):
 			return
 		if self.articles:
 			for item in self.articles:
-				line = line +1
-				if item and item.ref:
-					c = item.ref.replace(" ","").replace("-","").replace("_","").replace("/","").replace(".","")
-					code = frappe.db.get_value('Item', {"clean_manufacturer_part_number": c}, ['item_code'])
-					article = frappe.get_doc("Item",code)
-					if article:
-						# adresse magasin
-						if not article.table_adresse_magasin or (item.adr not in {a.adresse for a in article.table_adresse_magasin}):
-							row = article.append('table_adresse_magasin',{})
-							row.warehouse = self.stock
-							row.adresse = item.adr
-							try:
-								article.save()
-							except:
-								nothan.append(item)
-								errors += "Erreur article %d %s" % (line, item.ref)
-								
-						# prix
-						if item.publique:
-							price = frappe.get_all("Item Price",fields=["name"],filters={"min_qty":0,"item_code":article.name,"price_list":"PRIX PUBLIQUE"})
-							if price:
-								price = frappe.get_doc("Item Price",price[0].name)
-								price.price_list_rate = item.publique
-								price.min_qty = 0
+				try:
+					line = line +1
+					if item and item.ref:
+						c = item.ref.replace(" ","").replace("-","").replace("_","").replace("/","").replace(".","")
+						code = frappe.db.get_value('Item', {"clean_manufacturer_part_number": c}, ['item_code'])
+						article = frappe.get_doc("Item",code)
+						if article:
+							# adresse magasin
+							if not article.table_adresse_magasin or (item.adr not in {a.adresse for a in article.table_adresse_magasin}):
+								row = article.append('table_adresse_magasin',{})
+								row.warehouse = self.stock
+								row.adresse = item.adr
 								try:
-									price.save()
+									article.save()
 								except:
 									nothan.append(item)
 									errors += "Erreur article %d %s" % (line, item.ref)
-								#price.save()
-							else:
-								so = frappe.new_doc("Item Price")
-								so.item_code = article.item_code
-								so.price_list = "PRIX PUBLIQUE"
-								so.price_list_rate = item.publique
-								try:
-									so.save()
-								except:
-									nothan.append(item)
-									errors += "Erreur article %d %s" % (line, item.ref)
-						if item.gros:
-							price = frappe.get_all("Item Price",fields=["name"],filters={"min_qty":0,"item_code":article.name,"price_list":"PRIX EN GROS"})
-							if price:
-								price = frappe.get_doc("Item Price",price[0].name)
-								price.price_list_rate = item.gros
-								price.min_qty = 0
-								try:
-									price.save()
-								except:
-									nothan.append(item)
-									errors += "Erreur article %d %s" % (line, item.ref)
-							else:
-								so = frappe.new_doc("Item Price")
-								so.item_code = article.item_code
-								so.price_list = "PRIX EN GROS"
-								so.price_list_rate = item.gros
-								try:
-									so.save()
-								except:
-									nothan.append(item)
-									errors += "Erreur article %d %s" % (line, item.ref)
-						saved = saved+1
-						#article.save()
-						frappe.db.commit()
+
+							# prix
+							if item.publique:
+								price = frappe.get_all("Item Price",fields=["name"],filters={"min_qty":0,"item_code":article.name,"price_list":"PRIX PUBLIQUE"})
+								if price:
+									price = frappe.get_doc("Item Price",price[0].name)
+									price.price_list_rate = item.publique
+									price.min_qty = 0
+									try:
+										price.save()
+									except:
+										nothan.append(item)
+										errors += "Erreur article %d %s" % (line, item.ref)
+									#price.save()
+								else:
+									so = frappe.new_doc("Item Price")
+									so.item_code = article.item_code
+									so.price_list = "PRIX PUBLIQUE"
+									so.price_list_rate = item.publique
+									try:
+										so.save()
+									except:
+										nothan.append(item)
+										errors += "Erreur article %d %s" % (line, item.ref)
+							if item.gros:
+								price = frappe.get_all("Item Price",fields=["name"],filters={"min_qty":0,"item_code":article.name,"price_list":"PRIX EN GROS"})
+								if price:
+									price = frappe.get_doc("Item Price",price[0].name)
+									price.price_list_rate = item.gros
+									price.min_qty = 0
+									try:
+										price.save()
+									except:
+										nothan.append(item)
+										errors += "Erreur article %d %s" % (line, item.ref)
+								else:
+									so = frappe.new_doc("Item Price")
+									so.item_code = article.item_code
+									so.price_list = "PRIX EN GROS"
+									so.price_list_rate = item.gros
+									try:
+										so.save()
+									except:
+										nothan.append(item)
+										errors += "Erreur article %d %s" % (line, item.ref)
+							saved = saved+1
+							#article.save()
+							frappe.db.commit()
+				except:
+					errors += "Erreurs"
+
 		self.articles = nothan
 		frappe.msgprint("Done %d   <br>%s" % (saved,errors))
 		
