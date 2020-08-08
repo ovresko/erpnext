05acd2364af305868b61737a72d65f848b86ba79
ovresko <32199620+ovresko@users.noreply.github.com>
2020-08-08 12:18:35 +0100

    Update rapport_analyse_de_besoin.py

--- erpnext/stock/report/rapport_analyse_de_besoin/rapport_analyse_de_besoin.py
+++ erpnext/stock/report/rapport_analyse_de_besoin/rapport_analyse_de_besoin.py
@@ -157,18 +157,38 @@ def execute(filters=None):
 	mris = []
 
 	order_by_statement = "order by item_code"
-	items = frappe.db.sql(
-		"""
-		select
-			stock_uom,item_bloque, perfection,is_purchase_item,weight_per_unit,variant_of,has_variants,item_name, item_code, manufacturer,last_purchase_rate , manufacturer_part_no, item_group,last_purchase_devise,max_order_qty,max_ordered_variante
-		from `tabItem`
-		where disabled=0 and has_variants=0 {conditions}
-		{order_by_statement}
-		""".format(
-			conditions=get_conditions(filters),
-			order_by_statement=order_by_statement
-		),
-		filters, as_dict=1)
+	items = []
+	if filters.get("recu"):
+		r = filters.get("recu")
+		rec_items = frappe.get_all("Materiel Receipt Item", fields=["item_code"], filters={"parent":r})
+		rec_items_str = " ,".join(re.item_code for re in rec_items if re.item_code)
+		items = frappe.db.sql(
+			"""
+			select
+				stock_uom,item_bloque, perfection,is_purchase_item,weight_per_unit,variant_of,has_variants,item_name, item_code, manufacturer,last_purchase_rate , manufacturer_part_no, item_group,last_purchase_devise,max_order_qty,max_ordered_variante
+			from `tabItem`
+			where disabled=0 and has_variants=0 {conditions} and item_code in [{rec}]
+			{order_by_statement}
+			""".format(
+				conditions=get_conditions(filters),
+				rec=rec_items_str,
+				order_by_statement=order_by_statement
+			),
+			filters, as_dict=1)
+		
+	else:
+		items = frappe.db.sql(
+			"""
+			select
+				stock_uom,item_bloque, perfection,is_purchase_item,weight_per_unit,variant_of,has_variants,item_name, item_code, manufacturer,last_purchase_rate , manufacturer_part_no, item_group,last_purchase_devise,max_order_qty,max_ordered_variante
+			from `tabItem`
+			where disabled=0 and has_variants=0 {conditions}
+			{order_by_statement}
+			""".format(
+				conditions=get_conditions(filters),
+				order_by_statement=order_by_statement
+			),
+			filters, as_dict=1)
 	all_items = []
 	item_dc = {}
 	mcomplements = []
