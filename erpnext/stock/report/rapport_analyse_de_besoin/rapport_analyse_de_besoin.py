7b79b2737f4982b8c71567c864435c9df37c1e53
ovresko <32199620+ovresko@users.noreply.github.com>
2020-08-08 12:33:34 +0100

    Update rapport_analyse_de_besoin.py

--- erpnext/stock/report/rapport_analyse_de_besoin/rapport_analyse_de_besoin.py
+++ erpnext/stock/report/rapport_analyse_de_besoin/rapport_analyse_de_besoin.py
@@ -168,7 +168,7 @@ def execute(filters=None):
 			select
 				stock_uom,item_bloque, perfection,is_purchase_item,weight_per_unit,variant_of,has_variants,item_name, item_code, manufacturer,last_purchase_rate , manufacturer_part_no, item_group,last_purchase_devise,max_order_qty,max_ordered_variante
 			from `tabItem`
-			where disabled=0 and has_variants=0 {conditions} and item_code in [{rec}]
+			where disabled=0 and has_variants=0 {conditions} and item_code in ({rec})
 			{order_by_statement}
 			""".format(
 				conditions=get_conditions(filters),
