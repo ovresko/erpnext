0f422b1333a1d7e13ed5455fd508fa439a32144f
ovresko <32199620+ovresko@users.noreply.github.com>
2020-08-23 14:10:13 +0200

    Update etude_des_prix_de_vente.py

--- erpnext/selling/report/etude_des_prix_de_vente/etude_des_prix_de_vente.py
+++ erpnext/selling/report/etude_des_prix_de_vente/etude_des_prix_de_vente.py
@@ -418,7 +418,7 @@ def execute(filters=None):
 					if (not info or info[0] <= 0):
 						itr = """%s""" % (_price)
 					else:
-						itr = """[ %s %% ]&nbsp;&nbsp;<input placeholder='Prix %s' id='price_%s_%s' value='%s' style='color:black'></input>&nbsp;<input placeholder='Qts' id='qts_%s_%s' style='color:black;width:60px'></input><a  onClick="set_price_item('%s','%s')" type='a'  id='btn_%s_%s'> OK </a>&nbsp;	&nbsp;   %s	""" % (benefice,pl.name,mri.item_code,pl.name.replace(" ",""),new_taux,mri.item_code,pl.name.replace(" ",""),pl.name,mri.item_code,mri.item_code,pl.name.replace(" ",""),_price)
+						itr = """[ %s %% ]&nbsp;&nbsp;<input placeholder='Prix %s' id='price_%s_%s' value='%s' style='color:black'></input>&nbsp;<input placeholder='Qts' id='qts_%s_%s' style='color:black;width:60px'></input><a  onClick="set_price_item('%s','%s','%s')" type='a'  id='btn_%s_%s'> OK </a>&nbsp;	&nbsp;   %s	""" % (benefice,pl.name,mri.item_code,pl.name.replace(" ",""),new_taux,mri.item_code,pl.name.replace(" ",""),pl.name,mri.item_code,val_ttc,mri.item_code,pl.name.replace(" ",""),_price)
 				if itr:
 					row.append(itr)
 				else:
