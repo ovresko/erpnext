d22128bd7d576bf4b546b654aecf321560c008ce
ovresko <32199620+ovresko@users.noreply.github.com>
2020-09-09 13:24:59 +0200

    Update item.py

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -269,7 +269,7 @@ class Item(WebsiteGenerator):
 			if depot_parent:
 				warehouses= frappe.db.sql("""select name from `tabWarehouse` where parent_warehouse=%s""",(depot_parent),as_dict=True)
 				if warehouses:
-					qtotal = frappe.db.sql("""select sum(actual_qty) from  tabBin  where item_code=%s and warehouse in (%s)""" % ', '.join(['%s']*len(warehouses)),(self.item_code, tuple([w.name for w in warehouses])))
+					qtotal = frappe.db.sql("""select sum(actual_qty) from  tabBin  where item_code=%s and warehouse in (%s)""" % (self.item_code,', '.join(['%s']*len(warehouses))),tuple([w.name for w in warehouses]))
 					if qtotal:
 						self.qts_depot = qtotal[0][0]
 
