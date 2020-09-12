f463219f32fc89bd15135498cf15adfe6f14261b
ovresko <32199620+ovresko@users.noreply.github.com>
2020-09-12 14:18:06 +0200

    Update point_of_sale.js

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -2732,12 +2732,12 @@ class POSItems {
 			item.actual_qty = item.actual_qty  - item.reserved_qty;
 		 }
 		let actual_qty = '0';
-		if(item.actual_qty || item.qts_total){
+		if(item.actual_qty || item.qts_total || item.qts_depot){
 			price = '<span class="price-info" style="margin:0px;background-color: rgba(10, 154, 7, 0.8);border-radius: 0px;font-weight: bold;">' +price_list_rate +'</span>';
 		}
 		
 		if(show_qts_btn || ((item.actual_qty || 0) == 0 && item.qts_total && (item.qts_total || 0) != (item.actual_qty || 0))){
-			actual_qty = '<span title="' + tooltip +'" class="strong" style="color:#02AF22">Qts: '+(item.actual_qty || 0)+ '  / '+(item.qts_total || 0)+' </span>   <button data-item-code="'+item.item_code+'" data-label="stock" class="btn btn-default btn-xs btn-stock" style="margin-right: 5px;"><i class="fa fa-cubes"></i></button>';
+			actual_qty = '<span title="' + tooltip +'" class="strong" style="color:#02AF22">Qts: '+(item.actual_qty || 0)+ ' / ' +(item.qts_depot || 0) + '  / '+(item.qts_total || 0)+' </span>   <button data-item-code="'+item.item_code+'" data-label="stock" class="btn btn-default btn-xs btn-stock" style="margin-right: 5px;"><i class="fa fa-cubes"></i></button>';
 		}else{			
 			actual_qty = '<span  title="' + tooltip +'" class="strong" style="color:#02AF22">Qts: '+(item.actual_qty || 0)+ ' </span>  ';
 				
