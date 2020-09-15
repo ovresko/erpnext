28c04c7e188f7cca730c28d975d74315bbd6d45d
ovresko <32199620+ovresko@users.noreply.github.com>
2020-09-15 13:05:09 +0200

    Update catalogue_articles.js

--- erpnext/stock/report/catalogue_articles/catalogue_articles.js
+++ erpnext/stock/report/catalogue_articles/catalogue_articles.js
@@ -135,11 +135,6 @@ frappe.query_reports["Catalogue Articles"] = {
 			"fieldtype": "Select",
 			"options": ["","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
 			"default": ""
-		},{
-			"fieldname": "is_purchase",
-			"label": "Article d'achat",
-			"fieldtype": "Check",
-			"default": 1
 		},
 		{
 			"fieldname": "perfection",
