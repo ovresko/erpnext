66a251420f1ff7c6ee2bc16984d5f7a3c8e2c8a6
ovresko <32199620+ovresko@users.noreply.github.com>
2020-09-19 14:23:33 +0200

    Update item.py

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -1288,7 +1288,7 @@ def bulk_print_memberships(names):
 	frappe.local.response.filename = "{filename}.pdf".format(filename="catalogue".replace(" ", "-").replace("/", "-"))
 	frappe.local.response.filecontent = dignity_get_pdf(final_html, options=pdf_options) #get_pdf(final_html, pdf_options)
 	frappe.local.response.type = "download"
-	return frappe.local.response.filename
+	
 	
 def prepare_bulk_print_html(names):
 	#frappe.msgprint(names)
