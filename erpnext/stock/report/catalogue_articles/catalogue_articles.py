798103084aa8d055a202075fb760c9ae4e6a5632
ovresko <32199620+ovresko@users.noreply.github.com>
2020-09-20 10:59:36 +0200

    Update catalogue_articles.py

--- erpnext/stock/report/catalogue_articles/catalogue_articles.py
+++ erpnext/stock/report/catalogue_articles/catalogue_articles.py
@@ -196,7 +196,7 @@ def execute(filters=None):
 			desg = ""
 			if mri.has_variants:
 				#Version vehicule item
-				desg_version = frappe.db.sql("""select GROUP_CONCAT(distinct IFNULL(generation_vehicule,'A') SEPARATOR ', ') from `tabVersion vehicule item` where  parent=%s ;""",(mri.item_code), as_dict=1)
+				desg_version = frappe.db.sql("""select GROUP_CONCAT(distinct IFNULL(generation_vehicule,'A') SEPARATOR ', ') from `tabVersion vehicule item` where  parent='%s' ;""" % (mri.item_code), as_dict=1)
 				desg_generation = frappe.db.sql("""select GROUP_CONCAT(distinct IFNULL(nom_generation,'') SEPARATOR ', ') as name from `tabGeneration vehicule item` where  parent=%s ;""",(mri.item_code), as_dict=1)
 				desg_modele = frappe.db.sql("""select GROUP_CONCAT(distinct CONCAT(IFNULL(nom_marque,''),' ',IFNULL(nom_modele,'')) SEPARATOR ', ') as name from `tabModele vehicule item` where  parent=%s ;""",(mri.item_code), as_dict=1)
 				desg_marque = frappe.db.sql("""select GROUP_CONCAT(distinct IFNULL(marque,'') SEPARATOR ', ') as name from `tabMarque vehicule item` where  parent=%s ;""",(mri.item_code), as_dict=1)
