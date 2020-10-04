71c5cb190c2a42278ab6c091527f20c4968ace40
ovresko <32199620+ovresko@users.noreply.github.com>
2020-10-04 19:34:09 +0200

    Update rapport_analyse_depot.py

--- erpnext/stock/report/rapport_analyse_depot/rapport_analyse_depot.py
+++ erpnext/stock/report/rapport_analyse_depot/rapport_analyse_depot.py
@@ -211,8 +211,9 @@ def execute(filters=None):
 				_date = ""
 				date =""
 				qts_local = 0
-				bin = frappe.db.sql("""select actual_qty from `tabBin` where item_code = %s and warehouse = %s""", (mri.item_code, filters.get('warehouse')), as_dict=1)
-				qts_local = bin[0][0] if bin and bin[0] else 0
+				bin = frappe.db.sql("""select actual_qty from `tabBin` where item_code = %s and warehouse = %s limit 1""", (mri.item_code, filters.get('warehouse')), as_dict=1)
+				if bin and len(bin) > 0:
+					qts_local = bin[0][0] or 0
 				cmp = "%s CP" % mri.item_code if (mri.has_variants and mri.item_code in mcomplements) else mri.item_code
 				row = ["""<input type='button' onclick="erpnext.utils.open_item_info('%s', this)" value='info'>  </input> &nbsp;&nbsp;&nbsp; <button id='%s' onClick="demander_item('%s')" type='button'>Demander</button><input placeholder='Qts' id='input_%s' style='color:black'></input>""" % (mri.item_code,mri.item_code,mri.item_code,mri.item_code),
 				       cmp,
@@ -244,8 +245,10 @@ def execute(filters=None):
 				recom = 0
 				_date = ""
 				date =""
-				bin = frappe.db.sql("""select actual_qty from `tabBin` where item_code = %s and warehouse = %s""", (mri.item_code, filters.get('warehouse')), as_dict=1)
-				qts_local = bin[0][0] if bin and bin[0] else 0
+				qts_local=0
+				bin = frappe.db.sql("""select actual_qty from `tabBin` where item_code = %s and warehouse = %s limit 1""", (mri.item_code, filters.get('warehouse')), as_dict=1)
+				if bin and len(bin) > 0:
+					qts_local = bin[0][0] or 0
 				cmp = "%s CP" % mri.item_code if (mri.has_variants and mri.item_code in mcomplements) else mri.item_code
 				row = ["""<input type='button' onclick="erpnext.utils.open_item_info('%s', this)" value='info'>  </input> &nbsp;&nbsp;&nbsp; <button id='%s' onClick="demander_item('%s')" type='button'>Demander</button><input placeholder='Qts' id='input_%s' style='color:black'></input>""" % (mri.item_code,mri.item_code,mri.item_code,mri.item_code),
 				       cmp,
