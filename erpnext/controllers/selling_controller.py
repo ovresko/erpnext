51c3a6eb0b0f714e0e4cef144e2248249fa37364
ovresko <32199620+ovresko@users.noreply.github.com>
2020-10-05 20:29:28 +0200

    Update selling_controller.py

--- erpnext/controllers/selling_controller.py
+++ erpnext/controllers/selling_controller.py
@@ -158,8 +158,7 @@ class SellingController(StockController):
 
 	def validate_selling_price(self):
 		def throw_message(item_name, rate, ref_rate_field,idx=None):
-			msg = _("""Selling rate for item {0} is lower than its {1}. Selling rate should be atleast {2}""")
-				.format(item_name, ref_rate_field, rate)
+			msg = _("""Selling rate for item {0} is lower than its {1}. Selling rate should be atleast {2}""").format(item_name, ref_rate_field, rate)
 			frappe.throw(("Ligne : %s, " % idx) + msg)
 
 		if not frappe.db.get_single_value("Selling Settings", "validate_selling_price"):
