fec6a9d69ca187296de1480f6016660f0c5a6520
ovresko <32199620+ovresko@users.noreply.github.com>
2021-02-03 19:01:10 +0100

    Update etat_reliquats_clients.py

--- erpnext/selling/report/etat_reliquats_clients/etat_reliquats_clients.py
+++ erpnext/selling/report/etat_reliquats_clients/etat_reliquats_clients.py
@@ -68,7 +68,7 @@ def execute(filters=None):
 			"width": 120
 		})
 	
-	orders_items = frappe.db.sql(""" select 
+	orders_items = frappe.db.sql("""select 
 					mr.name as "Commande:Link/Sales Order:Data:50",
 					mr.delivery_date as "Date de livraison:Data:100",
 					mr.customer_name as "Client:Data:160",
@@ -80,13 +80,13 @@ def execute(filters=None):
 					mri.delivered_qty as "Qts livree:Data:100",
 					@reste :=(mri.qty - mri.delivered_qty) as "Reliquat:Data:100",
 					mr.status as "Status:Data:50"
-				from `tabSales Order` mr 
-				left join `tabSales Order Item` mri 
-				on mr.name = mri.parent
-				where mr.docstatus = 1 and mr.status != 'Closed' 
-				and mr.status != 'Completed' 
-				and mri.delivered_qty < mri.qty 
-				and (mr.customer=%(customer)s OR %(customer)s IS NULL OR %(customer)s ='NA') """),filters,as_dict=1)
+					from `tabSales Order` mr 
+					left join `tabSales Order Item` mri 
+					on mr.name = mri.parent
+					where mr.docstatus = 1 and mr.status != 'Closed' 
+					and mr.status != 'Completed' 
+					and mri.delivered_qty < mri.qty 
+					and (mr.customer=%(customer)s OR %(customer)s IS NULL OR %(customer)s ='NA') """,filters,as_dict=1)
 	
 	for item in orders_items:
 		row = [
