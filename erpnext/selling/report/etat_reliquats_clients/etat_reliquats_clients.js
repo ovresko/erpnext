3923d3545a0800a5119fddba99f6386dcc7d3410
ovresko <32199620+ovresko@users.noreply.github.com>
2021-02-03 18:48:23 +0100

    Update etat_reliquats_clients.js

--- erpnext/selling/report/etat_reliquats_clients/etat_reliquats_clients.js
+++ erpnext/selling/report/etat_reliquats_clients/etat_reliquats_clients.js
@@ -1,3 +1,23 @@
+
+function delete_item(data) {
+
+	var confirme = confirm("Confirmer l'operation ?");
+	if(!confirme){
+		return;
+	}
+	frappe.call({
+			method: "erpnext.stock.doctype.item.item.delete_order_item",
+			args: {
+				item_code: data
+			},
+			callback: function(r) {
+				if (r.message) {
+					alert(r.message);
+				}
+			}
+		});
+}
+
 frappe.query_reports["Etat Reliquats Clients"] = {
 "filters" : [
 {"fieldname": "customer",
