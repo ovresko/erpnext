3ae2dca626870c45f5250794370366c339a32581
ovresko <32199620+ovresko@users.noreply.github.com>
2021-03-07 13:28:57 +0100

    Update point_of_sale.js

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -19,57 +19,7 @@ frappe.pages['point-of-sale'].on_page_load = function(wrapper) {
 			frappe.set_route('pos');
 		}
 	});
-	
-	document.addEventListener("DOMContentLoaded", function() {
-	  var lazyloadImages;    
-	  console.log("lazy");
-	  if ("IntersectionObserver" in window) {
-		    console.log("window");
-	    lazyloadImages = document.querySelectorAll(".lazy");
-	    var imageObserver = new IntersectionObserver(function(entries, observer) {
-	      entries.forEach(function(entry) {
-		if (entry.isIntersecting) {
-		  var image = entry.target;
-		  image.src = image.dataset.src;
-		  image.classList.remove("lazy");
-		  imageObserver.unobserve(image);
-		}
-	      });
-	    });
 
-	    lazyloadImages.forEach(function(image) {
-	      imageObserver.observe(image);
-	    });
-	  } else {  
-	    var lazyloadThrottleTimeout;
-	    lazyloadImages = document.querySelectorAll(".lazy");
-
-	    function lazyload () {
-	      if(lazyloadThrottleTimeout) {
-		clearTimeout(lazyloadThrottleTimeout);
-	      }    
-
-	      lazyloadThrottleTimeout = setTimeout(function() {
-		var scrollTop = window.pageYOffset;
-		lazyloadImages.forEach(function(img) {
-		    if(img.offsetTop < (window.innerHeight + scrollTop)) {
-		      img.src = img.dataset.src;
-		      img.classList.remove('lazy');
-		    }
-		});
-		if(lazyloadImages.length == 0) { 
-		  document.removeEventListener("scroll", lazyload);
-		  window.removeEventListener("resize", lazyload);
-		  window.removeEventListener("orientationChange", lazyload);
-		}
-	      }, 20);
-	    }
-
-	    document.addEventListener("scroll", lazyload);
-	    window.addEventListener("resize", lazyload);
-	    window.addEventListener("orientationChange", lazyload);
-	  }
-	})
 
 };
 
@@ -3475,3 +3425,56 @@ class Payment {
 	}
 
 }
+$( document ).ready(function() {
+	   var lazyloadImages;    
+	  console.log("lazy");
+	  if ("IntersectionObserver" in window) {
+		    console.log("window");
+	    lazyloadImages = document.querySelectorAll(".lazy");
+	    var imageObserver = new IntersectionObserver(function(entries, observer) {
+	      entries.forEach(function(entry) {
+		if (entry.isIntersecting) {
+		  var image = entry.target;
+		  image.src = image.dataset.src;
+		  image.classList.remove("lazy");
+		  imageObserver.unobserve(image);
+		}
+	      });
+	    });
+
+	    lazyloadImages.forEach(function(image) {
+	      imageObserver.observe(image);
+	    });
+	  } else {  
+	    var lazyloadThrottleTimeout;
+	    lazyloadImages = document.querySelectorAll(".lazy");
+
+	    function lazyload () {
+	      if(lazyloadThrottleTimeout) {
+		clearTimeout(lazyloadThrottleTimeout);
+	      }    
+
+	      lazyloadThrottleTimeout = setTimeout(function() {
+		var scrollTop = window.pageYOffset;
+		lazyloadImages.forEach(function(img) {
+		    if(img.offsetTop < (window.innerHeight + scrollTop)) {
+		      img.src = img.dataset.src;
+		      img.classList.remove('lazy');
+		    }
+		});
+		if(lazyloadImages.length == 0) { 
+		  document.removeEventListener("scroll", lazyload);
+		  window.removeEventListener("resize", lazyload);
+		  window.removeEventListener("orientationChange", lazyload);
+		}
+	      }, 20);
+	    }
+
+	    document.addEventListener("scroll", lazyload);
+	    window.addEventListener("resize", lazyload);
+	    window.addEventListener("orientationChange", lazyload);
+	  }
+	 
+});
+	
+
