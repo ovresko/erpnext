6419dba89d1b652fd3104475cde28da1acf6ac1c
ovresko <32199620+ovresko@users.noreply.github.com>
2021-03-09 00:01:47 +0100

    Update point_of_sale.js

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -1765,7 +1765,7 @@ class POSItems {
 			<div class="vehicule-name-wrapper" style="display:flex;background-color:#F0FBFA;border-radius:20px">
 				 
 				<div style="width:20% ">
-				<img class="vehicule-image" src=""  loading="lazy" style="position: relative;top: 50%;transform: translateY(-50%);">
+				<img class="vehicule-image"   src=""  loading="lazy" style="position: relative;top: 50%;transform: translateY(-50%);">
 				</div>
 
 				<div style="width:15% ;padding:10px">	
@@ -2271,7 +2271,7 @@ class POSItems {
 		}
 
 		this.clusterize.update(row_items);
-		//loadLazzy();
+		loadLazzy();
 	}
 
 	filter_items({start=0, search_term='' }={}) {
@@ -2918,7 +2918,7 @@ class POSItems {
 							${!image ? `<span class="placeholder-text">
 									${frappe.get_abbr(item_title)}
 								</span>` : '' }
-							${image ? `<img  src="${image}" loading="lazy" alt="${item_title}">` : '' }
+							${image ? `<img class="lazy" data-src="${image}" loading="lazy" alt="${item_title}">` : '' }
 						</div>
 						${price}
 					</a>
@@ -2935,7 +2935,7 @@ class POSItems {
 					</div>
 					 
 					<div style="width:200px;padding-left:5px" >
-						<div style="margin-bottom:10px"><img  src="${item.fabricant_logo ||'#'}"  loading="lazy" width="80px" ></div>
+						<div style="margin-bottom:10px"><img  class="lazy" data-src="${item.fabricant_logo ||'#'}"  loading="lazy" width="80px" ></div>
 						<div>
 							<a class="btn-manufacturer" data-name="${item.manufacturer ||''}">${item.manufacturer ||''}</a>
 						</div>
@@ -3446,55 +3446,21 @@ class Payment {
 
 }
 function loadLazzy() {
- 
-	   var lazyloadImages;    
-	  
-	  if ("IntersectionObserver" in window) {
-		    
-	    lazyloadImages = document.querySelectorAll(".lazy");
-	    var imageObserver = new IntersectionObserver(function(entries, observer) {
-	      entries.forEach(function(entry) {
-		if (entry.isIntersecting) {
-		  var image = entry.target;
-		  image.src = image.dataset.src;
-		  image.classList.remove("lazy");
-		  imageObserver.unobserve(image);
-		}
-	      });
-	    });
-
-	    lazyloadImages.forEach(function(image) {
-	      imageObserver.observe(image);
-	    });
-	  } else {  
-	    var lazyloadThrottleTimeout;
-	    lazyloadImages = document.querySelectorAll(".lazy");
-
-	    function lazyload () {
-	      if(lazyloadThrottleTimeout) {
-		clearTimeout(lazyloadThrottleTimeout);
-	      }    
-
-	      lazyloadThrottleTimeout = setTimeout(function() {
-		var scrollTop = window.pageYOffset;
+	 var lazyloadImages;    
+	setTimeout(function(){ 
+	
+	lazyloadImages = document.querySelectorAll(".lazy");
 		lazyloadImages.forEach(function(img) {
-		    if(img.offsetTop < (window.innerHeight + scrollTop)) {
-		      img.src = img.dataset.src;
-		      img.classList.remove('lazy');
-		    }
-		});
-		if(lazyloadImages.length == 0) { 
-		  document.removeEventListener("scroll", lazyload);
-		  window.removeEventListener("resize", lazyload);
-		  window.removeEventListener("orientationChange", lazyload);
-		}
-	      }, 20);
-	    }
+				
+			      img.src = img.dataset.src;
+			      img.classList.remove('lazy');
+				
+			});
+		
+	}, 3000);
 
-	    document.addEventListener("scroll", lazyload);
-	    window.addEventListener("resize", lazyload);
-	    window.addEventListener("orientationChange", lazyload);
-	  }
+		
+ 
 	 
 }
 	
