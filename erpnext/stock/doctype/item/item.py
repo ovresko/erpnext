1f7ad2698b59b1c31a93c12b97372894bcac1221
ovresko <32199620+ovresko@users.noreply.github.com>
2021-03-21 22:42:02 +0100

    Update item.py

--- erpnext/stock/doctype/item/item.py
+++ erpnext/stock/doctype/item/item.py
@@ -198,7 +198,15 @@ class Item(WebsiteGenerator):
 		self.validate_item_defaults()
 		self.update_defaults_from_item_group()
 		self.validate_stock_for_has_batch_and_has_serial()
-		#if self.has_variants:
+		
+		if self.has_variants:
+			count = frappe.db.sql("""select count(name) as cnt from `tabItem` where variant_of='%s'""" % (self.name),as_dict=1)
+			if count:
+				self.nbr_variante = count[0]['cnt'] or 0
+		else:
+			count = frappe.db.sql("""select count(name) as cnt from `tabItem` where variant_of='%s'""" % (self.variant_of),as_dict=1)
+			if count:
+				self.nbr_variante = count[0]['cnt'] or 0
 		#	self.nbr_var = ''
 		#	vars = frappe.db.sql(''' select count(name) from `tabItem` where variant_of=%s ''',self.name)
 		#	if vars:
