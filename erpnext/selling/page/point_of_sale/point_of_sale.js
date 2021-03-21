b4b32d64635f5dfb7d1f54d7a162d5a7730415a9
ovresko <32199620+ovresko@users.noreply.github.com>
2021-03-21 23:50:16 +0100

    Update point_of_sale.js

--- erpnext/selling/page/point_of_sale/point_of_sale.js
+++ erpnext/selling/page/point_of_sale/point_of_sale.js
@@ -1,5 +1,10 @@
 /* global Clusterize */
 frappe.provide('erpnext.pos');
+
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.lazy/1.7.9/jquery.lazy.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.lazy/1.7.9/jquery.lazy.plugins.min.js"></script>
+
+
 /*
  * jQuery appear plugin
  *
@@ -10,97 +15,7 @@ frappe.provide('erpnext.pos');
  *
  * Version: 0.3.3
  */
-(function($) {
-  var selectors = [];
-
-  var check_binded = false;
-  var check_lock = false;
-  var defaults = {
-    interval: 250,
-    force_process: false
-  }
-  var $window = $(window);
-
-  var $prior_appeared;
-
-  function process() {
-    check_lock = false;
-    for (var index = 0; index < selectors.length; index++) {
-      var $appeared = $(selectors[index]).filter(function() {
-        return $(this).is(':appeared');
-      });
-
-      $appeared.trigger('appear', [$appeared]);
-
-      if ($prior_appeared) {
-        var $disappeared = $prior_appeared.not($appeared);
-        $disappeared.trigger('disappear', [$disappeared]);
-      }
-      $prior_appeared = $appeared;
-    }
-  }
-
-  // "appeared" custom filter
-  $.expr[':']['appeared'] = function(element) {
-    var $element = $(element);
-    if (!$element.is(':visible')) {
-      return false;
-    }
-
-    var window_left = $window.scrollLeft();
-    var window_top = $window.scrollTop();
-    var offset = $element.offset();
-    var left = offset.left;
-    var top = offset.top;
-
-    if (top + $element.height() >= window_top &&
-        top - ($element.data('appear-top-offset') || 0) <= window_top + $window.height() &&
-        left + $element.width() >= window_left &&
-        left - ($element.data('appear-left-offset') || 0) <= window_left + $window.width()) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  $.fn.extend({
-    // watching for element's appearance in browser viewport
-    appear: function(options) {
-      var opts = $.extend({}, defaults, options || {});
-      var selector = this.selector || this;
-      if (!check_binded) {
-        var on_check = function() {
-          if (check_lock) {
-            return;
-          }
-          check_lock = true;
-
-          setTimeout(process, opts.interval);
-        };
-
-        $(window).scroll(on_check).resize(on_check);
-        check_binded = true;
-      }
-
-      if (opts.force_process) {
-        setTimeout(process, opts.interval);
-      }
-      selectors.push(selector);
-      return $(selector);
-    }
-  });
-
-  $.extend({
-    // force elements's appearance check
-    force_appear: function() {
-      if (check_binded) {
-        process();
-        return true;
-      };
-      return false;
-    }
-  });
-})(jQuery);
+ 
 
 frappe.pages['point-of-sale'].on_page_load = function(wrapper) {
 	frappe.ui.make_app_page({
@@ -3549,17 +3464,8 @@ class Payment {
 
 function loadLazzy() {
 	console.log("lazy");
-	$('.lazy').appear(function() {
-		lazyloadImages = document.querySelectorAll(".lazy");
-		console.log("appear");
-		lazyloadImages.forEach(function(img) {
-			      console.log("img");
-			      img.src = img.dataset.src;
-			      img.classList.remove('lazy');
-
-			});
-	});
-
+ 	$('.lazy').Lazy();
+	
 	// $('.image-view-container').on('scroll',  function(){ 
 	// 	console.log("scroll");
 	  // 	var lazyloadImages;    
