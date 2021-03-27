d7617148d339c0b81e5cbcdd086dae65b54de366
ovresko <32199620+ovresko@users.noreply.github.com>
2021-03-27 17:05:05 +0100

    Update sales_analytics.py

--- erpnext/selling/report/sales_analytics/sales_analytics.py
+++ erpnext/selling/report/sales_analytics/sales_analytics.py
@@ -136,8 +136,9 @@ class Analytics(object):
 
 		self.entries = frappe.db.sql("""
 			select i.item_code as entity, i.item_name as entity_name, i.{value_field} as value_field, s.{date_field}, i.ref_fabricant, qb.qts_total, qb.qts_depot
-			from `tab{doctype} Item` i , `tab{doctype}` s
-			left join (select item_code, qts_total, qts_depot from `tabItem`) qb on (entity = qb.item_code)
+			from `tab{doctype} Item` i 
+			left join (select item_code, qts_total, qts_depot from `tabItem`) qb on (i.item_code = qb.item_code), 
+			`tab{doctype}` s
 			where s.name = i.parent and i.docstatus = 1 and s.company = %s
 			and s.{date_field} between %s and %s
 		"""
