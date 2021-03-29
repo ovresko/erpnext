bcf0b6b18ca57b28c38c6bd30a70a9b3c8fd4c12
ovresko <32199620+ovresko@users.noreply.github.com>
2021-03-29 18:35:27 +0200

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -345,7 +345,7 @@ def open_item_info(item_code):
 						<button  class="btn btn-default btn-sm  btn-etat-stock" data-item-code="{item_code}"  style="margin-right: 5px;">Etat Stock</button>
 						<button  class="btn btn-default btn-sm  btn-etat-val" data-item-code="{item_code}"  style="margin-right: 5px;">Valorisation</button>
 						<button  class="btn btn-default btn-sm  btn-print" data-item-code="{item_code}"  style="margin-right: 5px;">Exporter</button>
-						<button  class="btn btn-default btn-sm  btn-analyse" data-item-code="{item.variant_of}"  style="margin-right: 5px;">Analyse</button>
+						<button  class="btn btn-default btn-sm  btn-analyse" data-item-code="{variant_of}"  style="margin-right: 5px;">Analyse</button>
 <br>
 						<div class="etat-stock"></div>
 						<div class="etat-price"></div>
@@ -405,7 +405,8 @@ def open_item_info(item_code):
 						<label>Composants </label>
 						<div>{composant_text}</div>
 						<hr>""".format(item_name=item.item_name,image=image,item_code=item_code,manufacturer_part_no=item.manufacturer_part_no
-							    ,oem_text=item.oem_text,manufacturer=item.manufacturer,fabricant_logo=fabricant_logo,critere_text=item.critere_text,articles_text=item.articles_text,composant_text=item.composant_text)
+							    ,oem_text=item.oem_text,manufacturer=item.manufacturer,fabricant_logo=fabricant_logo,critere_text=item.critere_text,
+							       articles_text=item.articles_text,composant_text=item.composant_text,variant_of= item.variant_of)
 		d = frappe.msgprint(_modal)
 		return d
 		
