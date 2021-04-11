cb4907253799269c880b5b6ee16615a2b76f1bdd
ovresko <32199620+ovresko@users.noreply.github.com>
2021-04-11 12:22:20 +0100

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -442,14 +442,15 @@ def get_item_info(item_code,price_list):
 @frappe.whitelist()
 def print_address_magasin(items,qts,pos_profile,customer):
 	items = items.split(",")
-	frappe.msgprint(items)
+	#frappe.msgprint(items)
 	qts = qts.split(",")
 	warehouse = frappe.get_value("POS Profile",pos_profile,"warehouse")
 	result = {}
 	failed = ""
 	if items:
 		for idx, item in enumerate(items):
-			
+			if item:
+				item = item.replace("[").replace("]")
 			q = qts[idx]
 			adr = frappe.db.get_value("Adresse Magasin", {"parent": item,"warehouse":warehouse}, 'adresse')
 			fabricant = frappe.db.get_value("Item", {"item_code": item}, 'manufacturer')
@@ -460,7 +461,8 @@ def print_address_magasin(items,qts,pos_profile,customer):
 					       		"qts":q,
 					       		"adr":adr,
 							"fabricant": fabricant,
-							"ref":ref
+							"ref":ref,
+							"item": item
 						}
 					      })
 			else:
@@ -469,7 +471,8 @@ def print_address_magasin(items,qts,pos_profile,customer):
 					       		"qts":q,
 					       		"adr":"Introuvable",
 							"fabricant": fabricant,
-							"ref":ref
+							"ref":ref,
+							"item": item
 						}
 					      })
 	else:
@@ -506,10 +509,10 @@ def prepare_bulk_print_html(names,customer,warehouse):
 	<p style="text-align:center">{{warehouse}}</p>
 	<p style="text-align:center">Client : {{customer}}</p>
 	
-	{% for sc in names %}<small>{{sc}} : <span style="font-weight:bold"">{{names[sc].qts}} <span> ************************ <span>{{names[sc].adr}}<span>
+	{% for sc in names %}<small>{{sc}} {{names[sc].item}} : <span style="font-weight:bold"">{{names[sc].qts}} <span> ************************ <span>{{names[sc].adr}}<span>
 	</small><br>{{names[sc].fabricant}} / {{names[sc].ref}}<br>----------------------------------------------------------<br>{% endfor %}
 	</div><body>
-	{{names}}
+	
 	</html>
 	""", {"names":names,"warehouse":warehouse,"customer":customer})
 	return final_html
